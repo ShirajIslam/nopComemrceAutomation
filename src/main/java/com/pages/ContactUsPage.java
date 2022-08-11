129e3eacce90465aa5f7466848f0535fc7e6d583
ShirajIslam <shiraz2704@gmail.com>
2022-08-11 20:32:45 +0600

    contact us feature added

--- src/main/java/com/pages/ContactUsPage.java
+++ src/main/java/com/pages/ContactUsPage.java
@@ -1,2 +1,46 @@
-package com.pages;public class ContactUsPage {
-}
+package com.pages;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+    public class ContactUsPage {
+
+        private WebDriver driver;
+
+        private By name1 = By.id("FullName");
+        private By email = By.id("Email");
+        private By messageText = By.id("Enquiry");
+        private By sendButton = By.xpath("//button[contains(@class,'contact-us-button')]");
+        private By successMessg = By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[2]");
+
+        public ContactUsPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public String getContactUsPageTitle() {
+            return driver.getTitle();
+        }
+
+        public void fillContactUsForm(String name, String emailId, String message) {
+            driver.findElement(name1).sendKeys(name);
+            driver.findElement(email).sendKeys(emailId);
+            driver.findElement(messageText).sendKeys(message);
+        }
+
+        public void clickSend() {
+            driver.findElement(sendButton).click();
+        }
+
+        public String getSuccessMessg() {
+            return driver.findElement(successMessg).getText();
+        }
+
+
+
+
+
+    }
+
--- src/test/java/stepDefinitions/ContactUsSteps.java
+++ src/test/java/stepDefinitions/ContactUsSteps.java
@@ -1,2 +1,50 @@
-package stepDefinitions;public class ContactUsSteps {
+package stepDefinitions;
+import com.pages.ContactUsPage;
+import com.qa.factory.DriverFactory;
+import com.qa.util.ExcelReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.formula.functions.Index;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class ContactUsSteps {
+    private ContactUsPage contactUsPage=new ContactUsPage(DriverFactory.getDriver());
+    @Given("user navigates to contact us page")
+    public void user_navigates_to_contact_us_page() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        DriverFactory.getDriver().get("https://localhost:44369/contactus");
+        }
+    @When("user fills the form from given sheetname {string} and rownumber {int}")
+    public void user_fills_the_form_from_given_sheetname_and_rownumber(String sheetName, Integer rowNumber) throws IOException, InvalidFormatException {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        ExcelReader reader=new ExcelReader();
+        List<Map<String,String>> testData=reader.getData("C:\\Users\\BS969\\eclipse-workspace\\CucumberPOMSeries\\src\\test\\resources/Automation.xlsx", sheetName);
+        String name= testData.get(rowNumber).get("name");
+        String email=testData.get(rowNumber).get("email");
+        String message=testData.get(rowNumber).get("message");
+        contactUsPage.fillContactUsForm(name,email,message);
+    }
+    @When("user clicks on send button")
+    public void user_clicks_on_send_button() throws InterruptedException {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        //Thread.sleep(2000);
+        contactUsPage.clickSend();
+    }
+    @Then("it shows a successful message {string}")
+    public void it_shows_a_successful_message(String expSuccessMessage) {
+        // Write code here that turns the phrase above into concrete actions
+
+       // throw new io.cucumber.java.PendingException();
+    String actualSuccMessg= contactUsPage.getSuccessMessg();
+        Assert.assertEquals(actualSuccMessg,expSuccessMessage);
+    }
 }
